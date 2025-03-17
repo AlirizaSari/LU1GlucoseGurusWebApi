864fcc440ea4d82ae12b88e730ccb11406c7aad6
AlirizaSari <AlisSari55@hotmail.com>
2025-03-17 18:07:06 +0100

    Added WebApi Project

---
+++ GlucoseGurusWebApi.WebApi/Controllers/AccountManagementController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+[ApiController]
+[Route("account/[controller]")]
+public class AccountManagementController : ControllerBase
+{
+    private readonly ILogger<AccountManagementController> logger;
+    private readonly UserManager<IdentityUser> userManager;
+    private readonly SignInManager<IdentityUser> signInManager;
+    private readonly RoleManager<IdentityUser> roleManager;
+
+    public AccountManagementController(ILogger<AccountManagementController> logger, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, RoleManager<IdentityUser> roleManager)
+    {
+        this.logger = logger;
+        this.userManager = userManager;
+        this.signInManager = signInManager;
+        this.roleManager = roleManager;
+    }
+}
---
+++ GlucoseGurusWebApi.WebApi/Program.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services
+    .AddAuthentication(BearerTokenDefaults.AuthenticationScheme)
+    .AddBearerToken();
+
+builder.Services
+    .AddIdentityApiEndpoints<IdentityUser>(options =>
+    {
+        options.User.RequireUniqueEmail = true;
+        options.Password.RequiredLength = 10;
+        options.Password.RequireDigit = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireNonAlphanumeric = true;
+    })
+    .AddRoles<IdentityRole>()
+    .AddDapperStores(options =>
+    {
+        options.ConnectionString = builder.Configuration["SqlConnectionString"];
+    });
+
+builder.Services.Configure<BearerTokenOptions>(options =>
+{
+    options.BearerTokenExpiration = TimeSpan.FromMinutes(60);
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy =>
+    {
+        policy.RequireClaim("admin");
+    });
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.Configure<RouteOptions>(o => o.LowercaseUrls = true);
+
+var sqlConnectionString = builder.Configuration["SqlConnectionString"];
+var sqlConnectionStringFound = !string.IsNullOrWhiteSpace(sqlConnectionString);
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddTransient<IAuthenticationService, AspNetIdentityAuthenticationService>();
+
+var app = builder.Build();
+
+app.MapGet("/", () => $"The API is up 🚀\nConnection string found: {(sqlConnectionStringFound ? "✅" : "❌")}");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGroup("/account")
+    .MapIdentityApi<IdentityUser>();
+
+app.MapPost("/account/logout", async (SignInManager<IdentityUser> signInManager) =>
+{
+    await signInManager.SignOutAsync();
+    return Results.Ok();
+})
+.RequireAuthorization();
+
+app.MapControllers()
+    .RequireAuthorization();
+
+app.Run();
