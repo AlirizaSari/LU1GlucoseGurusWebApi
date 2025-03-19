c846a203b354aa0c9ae3fc1b9a312e7c61c218af
AlirizaSari <AlisSari55@hotmail.com>
2025-03-19 16:04:55 +0100

    Added Interface for Patient

---
+++ GlucoseGurusWebApi.WebApi/Repositories/IPatientRepository.cs
@@ -0,0 +1,13 @@
+﻿using GlucoseGurusWebApi.WebApi.Models;
+
+namespace GlucoseGurusWebApi.WebApi.Repositories
+{
+    public interface IPatientRepository
+    {
+        Task<Patient> InsertAsync(Patient patient);
+        Task<Patient?> ReadAsync(Guid id);
+        Task<IEnumerable<Patient>> ReadAllAsync();
+        Task UpdateAsync(Patient patient);
+        Task DeleteAsync(Guid id);
+    }
+}
