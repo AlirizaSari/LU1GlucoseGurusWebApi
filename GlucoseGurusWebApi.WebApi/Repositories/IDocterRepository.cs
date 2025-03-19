c4a66406f4f2a8796e2de0cd1cd927c0f5ef4784
AlirizaSari <AlisSari55@hotmail.com>
2025-03-19 16:38:18 +0100

    Added Interface for Docter

---
+++ GlucoseGurusWebApi.WebApi/Repositories/IDocterRepository.cs
@@ -0,0 +1,13 @@
+﻿using GlucoseGurusWebApi.WebApi.Models;
+
+namespace GlucoseGurusWebApi.WebApi.Repositories
+{
+    public interface IDocterRepository
+    {
+        Task<Docter> InsertAsync(Docter docter);
+        Task<Docter?> ReadAsync(Guid id);
+        Task<IEnumerable<Docter>> ReadAllAsync();
+        Task UpdateAsync(Docter docter);
+        Task DeleteAsync(Guid id);
+    }
+}
