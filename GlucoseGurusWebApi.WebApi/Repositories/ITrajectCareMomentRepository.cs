08a4be1257889e9c969c8ed118047d69e6e1fcb1
AlirizaSari <AlisSari55@hotmail.com>
2025-03-21 00:08:22 +0100

    Added Interface for TrajectCareMoment

---
+++ GlucoseGurusWebApi.WebApi/Repositories/ITrajectCareMomentRepository.cs
@@ -0,0 +1,13 @@
+﻿using GlucoseGurusWebApi.WebApi.Models;
+
+namespace GlucoseGurusWebApi.WebApi.Repositories
+{
+    public interface ITrajectCareMomentRepository
+    {
+        Task<TracjetCareMoment> InsertAsync(TracjetCareMoment tracjetCareMoment);
+        Task<TracjetCareMoment?> ReadAsync(Guid trajectId, Guid careMomentId);
+        Task<IEnumerable<TracjetCareMoment>> ReadAllAsync();
+        Task UpdateAsync(TracjetCareMoment tracjetCareMoment);
+        Task DeleteAsync(Guid trajectId, Guid careMomentId);
+    }
+}
