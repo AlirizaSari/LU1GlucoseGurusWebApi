f993c9f00feb4ec0d6b3513e1995e0c6a45973a9
AlirizaSari <AlisSari55@hotmail.com>
2025-03-21 00:08:37 +0100

    Added Repository for TrajectCareMoment

---
+++ GlucoseGurusWebApi.WebApi/Repositories/SqlTrajectCareMomentRepository.cs
@@ -0,0 +1,57 @@
+﻿using Dapper;
+using GlucoseGurusWebApi.WebApi.Models;
+using Microsoft.Data.SqlClient;
+
+namespace GlucoseGurusWebApi.WebApi.Repositories
+{
+    public class SqlTrajectCareMomentRepository : ITrajectCareMomentRepository
+    {
+        private readonly string sqlConnectionString;
+
+        public SqlTrajectCareMomentRepository(string sqlConnectionString)
+        {
+            this.sqlConnectionString = sqlConnectionString;
+        }
+
+        public async Task<TracjetCareMoment> InsertAsync(TracjetCareMoment tracjetCareMoment)
+        {
+            using (var sqlConnection = new SqlConnection(sqlConnectionString))
+            {
+                var trajectCareMomentId = await sqlConnection.ExecuteAsync("INSERT INTO [TrajectCareMoment] (TrajectId, CareMomentId, Name, Step) VALUES (@TrajectId, @CareMomentId, @Name, @Step)", tracjetCareMoment);
+                return tracjetCareMoment;
+            }
+        }
+
+        public async Task<TracjetCareMoment?> ReadAsync(Guid trajectId, Guid careMomentId)
+        {
+            using (var sqlConnection = new SqlConnection(sqlConnectionString))
+            {
+                return await sqlConnection.QueryFirstOrDefaultAsync<TracjetCareMoment>("SELECT * FROM [TrajectCareMoment] WHERE TrajectId = @TrajectId AND CareMomentId = @CareMomentId", new { TrajectId = trajectId, CareMomentId = careMomentId });
+            }
+        }
+
+        public async Task<IEnumerable<TracjetCareMoment>> ReadAllAsync()
+        {
+            using (var sqlConnection = new SqlConnection(sqlConnectionString))
+            {
+                return await sqlConnection.QueryAsync<TracjetCareMoment>("SELECT * FROM [TrajectCareMoment]");
+            }
+        }
+
+        public async Task UpdateAsync(TracjetCareMoment tracjetCareMoment)
+        {
+            using (var sqlConnection = new SqlConnection(sqlConnectionString))
+            {
+                await sqlConnection.ExecuteAsync("UPDATE [TrajectCareMoment] SET Name = @Name, Step = @Step WHERE TrajectId = @TrajectId AND CareMomentId = @CareMomentId", tracjetCareMoment);
+            }
+        }
+
+        public async Task DeleteAsync(Guid trajectId, Guid careMomentId)
+        {
+            using (var sqlConnection = new SqlConnection(sqlConnectionString))
+            {
+                await sqlConnection.ExecuteAsync("DELETE FROM [TrajectCareMoment] WHERE TrajectId = @TrajectId AND CareMomentId = @CareMomentId", new { TrajectId = trajectId, CareMomentId = careMomentId });
+            }
+        }
+    }
+}
