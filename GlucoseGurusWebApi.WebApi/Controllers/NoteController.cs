82a355b49e7a25df64b7fabba7d9c901e9ecea8e
Reinout <94532201+ReinoutvdG@users.noreply.github.com>
2025-03-20 22:17:18 +0100

    Notes created
    aangemaakt van de notes repository, interface en controller, er moeten eventueel nog meer safety checks toegevoegd worden maar het

---
+++ GlucoseGurusWebApi.WebApi/Controllers/NoteController.cs
@@ -0,0 +1,91 @@
+using GlucoseGurusWebApi.WebApi.Models;
+using GlucoseGurusWebApi.WebApi.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlucoseGurusWebApi.WebApi.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("notes")]
+    public class NoteController : ControllerBase
+    {
+        private readonly ILogger<NoteController> _logger;
+        private readonly INoteRepository _NoteRepository;
+        private readonly IAuthenticationService _authenticationService;
+
+        public NoteController(INoteRepository NoteRepository, IAuthenticationService authenticationService, ILogger<NoteController> logger)
+        {
+            _NoteRepository = NoteRepository;
+            _authenticationService = authenticationService;
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "ReadNotes")]
+        public async Task<ActionResult<IEnumerable<Note>>> Get()
+        {
+            var userId = _authenticationService.GetCurrentAuthenticatedUserId();
+            if (userId == null)
+                return Unauthorized();
+
+            var Notes = await _NoteRepository.ReadAllAsync();
+            return Ok(Notes);
+        }
+
+        [HttpGet("{NoteId}", Name = "ReadNote")]
+        public async Task<ActionResult<Note>> Get(Guid NoteId)
+        {
+            var userId = _authenticationService.GetCurrentAuthenticatedUserId();
+            if (userId == null)
+                return Unauthorized();
+
+            var Note = await _NoteRepository.ReadAsync(NoteId);
+            if (Note == null)
+                return NotFound($"Note does not exist.");
+
+            return Ok(Note);
+        }
+
+        [HttpPost(Name = "CreateNote")]
+        public async Task<ActionResult<Note>> Add(Note newNote)
+        {
+            var userId = _authenticationService.GetCurrentAuthenticatedUserId();
+            if (userId == null)
+                return Unauthorized();
+
+            var Note = await _NoteRepository.InsertAsync(newNote);
+            return CreatedAtRoute("ReadNote", new { NoteId = Note.Id }, Note);
+        }
+
+        [HttpPut("{NoteId}", Name = "UpdateNote")]
+        public async Task<ActionResult> Update(Guid NoteId, Note updatedNote)
+        {
+            var userId = _authenticationService.GetCurrentAuthenticatedUserId();
+            if (userId == null)
+                return Unauthorized();
+
+            var Note = await _NoteRepository.ReadAsync(NoteId);
+            if (Note == null)
+                return NotFound($"Note does not exist.");
+
+            updatedNote.Id = NoteId;
+            await _NoteRepository.UpdateAsync(updatedNote);
+            return Ok(updatedNote);
+        }
+
+        [HttpDelete("{NoteId}", Name = "DeleteNote")]
+        public async Task<ActionResult> Delete(Guid NoteId)
+        {
+            var userId = _authenticationService.GetCurrentAuthenticatedUserId();
+            if (userId == null)
+                return Unauthorized();
+
+            var Note = await _NoteRepository.ReadAsync(NoteId);
+            if (Note == null)
+                return NotFound($"Note does not exist.");
+
+            await _NoteRepository.DeleteAsync(NoteId);
+            return Ok();
+        }
+    }
+}
